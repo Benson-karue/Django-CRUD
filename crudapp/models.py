dbf0efdafe5d62f43737cb945536566d614b52ef
Benson-karue <127722249+Benson-karue@users.noreply.github.com>
2023-04-20 07:56:32 +0300

    Add files via upload

---
+++ crudapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Student(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    class Meta:
+        db_table = 'Student'
+
